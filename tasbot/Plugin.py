9d0397f9abbe94ea293455f1180535aeb3e24f93
Rene Milk <rene.milk@uni-muenster.de>
2011-04-28 18:20:58 +0200

    greatly simplify pluginhandler

--- tasbot/Plugin.py
+++ tasbot/Plugin.py
@@ -124,60 +124,32 @@ class PluginHandler(object):
 			return
 		Log.loaded("Plugin " + name)
 
+	def forall(self,func_name,*args):
+		Log.Info( 'forall %s'%func_name)
+		for name,plugin in filter(lambda (name,plugin): func_name in dir(plugin), self.plugins.iteritems() ):
+			try:
+				func = getattr(plugin,func_name)
+				func( *args )
+			except SystemExit:
+				raise SystemExit(0)
+			except Exception,e :
+				Log.Error("PLUGIN %s ERROR calling  %s"%(func_name,name))
+				Log.Except( e )
+				
 	def onconnected(self):
-		for plugin in self.plugins:
-			try:
-				if "onconnected" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onconnected()
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
-
+		self.forall( "onconnected" )
+			
 	def ondisconnected(self):
-		for plugin in self.plugins:
-			try:
-				if "ondisconnected" in dir(self.plugins[plugin]):
-					self.plugins[plugin].ondisconnected()
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
-
+		self.forall( "ondisconnected")
+		
 	def onmotd(self,content):
-		for plugin in self.plugins:
-			try:
-				if "onmotd" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onmotd(content)
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
+		self.forall( "onmotd", content)
 
 	def onsaid(self,channel,user,message):
-		for plugin in self.plugins:
-			try:
-				if "onsaid" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onsaid(channel,user,message)
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
+		self.forall( "onsaid",channel,user,message)
 
 	def onsaidex(self,channel,user,message):
-		for plugin in self.plugins:
-			try:
-				if "onsaidex" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onsaidex(channel,user,message)
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
+		self.forall( "onsaidex",channel,user,message)
 
 	def onsaidprivate(self,user,message):
 		args = message.split(" ")
@@ -204,70 +176,19 @@ class PluginHandler(object):
 				Log.bad("Unloadplugin failed")
 				Log.Error( traceback.print_exc() )
 
-		for plugin in self.plugins:
-			try:
-				if "onsaidprivate" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onsaidprivate(user,message)
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
+		self.forall( "onsaidprivate",user,message)
 
 	def onloggedin(self,socket):
-		for plugin in self.plugins:
-			try:
-				if "onloggedin" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onloggedin(socket)
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
+		self.forall( "onloggedin",socket)
 
 	def onpong(self):
-		for plugin in self.plugins:
-			try:
-				if "onpong" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onpong()
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
-
+		self.forall( "onpong" )
+		
 	def oncommandfromserver(self,command,args,socket):
-		for plugin in self.plugins:
-			try:
-				if "oncommandfromserver" in dir(self.plugins[plugin]):
-					
-					self.plugins[plugin].oncommandfromserver(command,args,socket)
-					
-			except SystemExit:
-				raise SystemExit(0)	
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
+		self.forall( "oncommandfromserver",command,args,socket)
 
 	def onexit(self):
-		for plugin in self.plugins:
-			try:
-				if "onexit" in dir(self.plugins[plugin]):
-					self.plugins[plugin].onexit()
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
-
+		self.forall( "onexit" )
+		
 	def ondisconnected(self):
-		for plugin in self.plugins:
-			try:
-				if "ondisconnected" in dir(self.plugins[plugin]):
-					self.plugins[plugin].ondisconnected()
-			except SystemExit:
-				raise SystemExit(0)
-			except:
-				Log.Error("PLUGIN ERROR")
-				Log.Error( traceback.print_exc() )
-
+		self.forall( "ondisconnected" )

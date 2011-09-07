7df71f2f93186290c38ecb2367aa62d43d497265
Rene Milk <rene.milk@uni-muenster.de>
2011-09-07 12:08:05 +0200

    add module versioning and check functions

--- tasbot/__init__.py
+++ tasbot/__init__.py
@@ -1,1 +1,26 @@
 from Main import MainApp as DefaultApp
+import sys
+version=(0,2,2)
+
+#pretty sure there's buitins for this but I couldn't find them
+def _greater(a,b):
+	return cmp(a,b) > 0
+def _less(a,b):
+	return cmp(a,b) < 0
+	
+def _compare(vtuple,op):
+	for i in range(len(vtuple)):
+		if op(version[i], vtuple[i]):
+			return False
+	return True
+	
+def check_min_version(vtuple):
+	if not _compare(vtuple,_less):
+		print('tasbot version %s does not match minimum requirement %s'%(str(version),str(vtuple)))
+		sys.exit(1)
+	
+def check_max_version(vtuple):
+	if not _compare(vtuple,_greater):
+		print('tasbot version %s exceeds maximum requirement %s'%(str(version),str(vtuple)))
+		sys.exit(1)
+
